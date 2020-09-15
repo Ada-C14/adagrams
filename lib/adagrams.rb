6f3bf18ae10812f6df3ea58022c4a7bcab2f32fe
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-14 19:01:29 -0700

    Moved draw_letters and letter pool data structure from wave-1-game.rb to lib/adagrams.rb.

---
+++ lib/adagrams.rb
@@ -0,0 +1,39 @@
+ALL_LETTERS = {
+    A: 9,
+    B: 2,
+    C: 2,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
+}
+
+def draw_letters
+  all_letters = []
+
+  ALL_LETTERS.each do |keys, values|
+    values.times do
+      all_letters << keys.to_s
+    end
+  end
+
+  return all_letters.shuffle.first(10)
+end
