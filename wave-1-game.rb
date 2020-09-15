573c2963fc2544acd0a0fb906f7c9419efb4405f
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-15 16:17:22 -0700

    Removed wave 1 method from wave-1-game.rb (all methods in lib/adagrams.rb).

--- wave-1-game.rb
+++ wave-1-game.rb
@@ -1,34 +1,5 @@
 require_relative 'lib/adagrams'
 
-ALL_LETTERS = {
-    A: 9,
-    B: 2,
-    C: 2,
-    D: 4,
-    E: 12,
-    F: 2,
-    G: 3,
-    H: 2,
-    I: 9,
-    J: 1,
-    K: 1,
-    L: 4,
-    M: 2,
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1
-}
-
 def display_welcome_message
   puts "Welcome to Adagrams!"
   puts "Let's draw 10 letters from the letter pool..."
@@ -43,20 +14,6 @@ def run_game
   display_welcome_message
   display_drawn_letters(draw_letters)
 end
-
-def draw_letters
-  all_letters = []
-
-  ALL_LETTERS.each do |keys, values|
-    values.times do
-      all_letters << keys.to_s
-    end
-  end
-
-  return all_letters.shuffle.first(10)
-end
-
-p draw_letters
 
 run_game
 
