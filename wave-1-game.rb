ccde24168a8ed1ccc43293256c1f89ce154ef2a8
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-14 18:56:49 -0700

    Wave 1 implemented (array of 10 chars returned).

--- wave-1-game.rb
+++ wave-1-game.rb
@@ -1,5 +1,33 @@
 require_relative 'lib/adagrams'
 
+ALL_LETTERS = {
+    A: 9,
+    B: 2,
+    C: 2,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
+}
+
 def display_welcome_message
   puts "Welcome to Adagrams!"
   puts "Let's draw 10 letters from the letter pool..."
@@ -16,8 +44,18 @@ def run_game
 end
 
 def draw_letters
+  all_letters = []
 
+  ALL_LETTERS.each do |keys, values|
+    values.times do
+      all_letters << keys.to_s
+    end
+  end
+
+  return all_letters.shuffle.first(10)
 end
+
+p draw_letters
 
 run_game
 
