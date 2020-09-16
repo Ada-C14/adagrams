4e6387b4eb8b350bbc5d128d3084bc9eb45e2f2c
Sophie Messing <sophie.e.messing@gmail.com>
2020-09-15 22:37:54 -0700

    created a hash method to count and compare letter frequencies in words vs. letters

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -38,28 +38,69 @@ distribution = {
 
   letters = []
 
-  # distribution_array.shuffle!
-  # letters << distribution_array.first(10)
-  # letters.flatten!
-
-p distribution_array.length
-
   10.times do
-    index = rand(0..distribution_array.length)
+    index = rand(0...distribution_array.length)
     letters << distribution_array[index]
     distribution_array.delete_at(index)
   end
 
-p distribution_array.length
-
-
   return letters
 end
 
-puts draw_letters
+
+def make_count_hash(array)
+  hash = Hash.new(0)
+  array.each do |letter|
+    hash[letter] += 1
+  end
+  return hash
+end
 
 
+letters = draw_letters
+letter_hash = make_count_hash(letters)
+
+p letters
+
+word = gets.chomp.upcase!
+word = word.split('')
+word_hash = make_count_hash(word)
+
+puts word_hash
+puts letter_hash
+
+def uses_available_letters?(word_hash, letter_hash)
+  word_hash.each do |letter, count|
+    if letter_hash[letter] < count
+      return false
+    end
+  end
+  return true
+end
+
+puts uses_available_letters?(word_hash, letter_hash)
+
+# letters = draw_letters
+# hand_hash = hand_letter_count_hash(letters)
+# word_hash = word_letter_count_hash(word)
+#
 
 
+# def uses_available_letters?(word, letters_in_hand)
+#   letters_in_hand.each_with_index do |letter_in_hand, i|
+#     if word.upcase.include?(letter_in_hand)
+#       letters_in_hand.delete_at(i)
+#     end
+#   end
+#   return letters_in_hand.length == 10 - word.length
+# end
+#
 
+# letters_in_hand = draw_letters
 
+# puts "here are your available letters!"
+# p letters_in_hand
+#
+# puts "try a word"
+#
+# puts uses_available_letters?(word, letters_in_hand)
