6743f7430b8ac5285428c4ec79e1c92cbf39dc85
Rachael Gomez <gomezrc1220@gmail.com>
2020-09-16 10:59:42 -0700

    added data structure for score chart

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -55,3 +55,15 @@ def uses_available_letters?(input, letters_in_hand)
   end
 end
 
+def score_word(word)
+  score_chart = {%w[A, E, I, O, U, L, N, R, S, T] => 1,
+                 %w[D, G] => 2,
+                 %w[B, C, M, P ] => 3,
+                 %w[F, H, V, W, Y] => 4,
+                 %w[K] => 5,
+                 %w[J, X] => 8,
+                 %w[Q, Z] => 10}
+  print score_chart
+end
+word = "ada"
+print score_word(word)
