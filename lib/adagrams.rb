96792b9b88aa64fbbfb3490cd2f6859d3704f8d1
Anna Kim <anna.kim93@gmail.com>
2020-09-16 11:41:32 -0400

    completed wave 3 - created method score_word

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,14 +1,3 @@
-# Our first job is to build a hand of 10 letters for the user. To do so, add a method called draw_letters in adagrams.rb. This method should have the following properties:
-
-# No parameters
-# Each string should contain exactly one letter
-# These represent the hand of letters that the player has drawn
-# The letters should be randomly drawn from a pool of letters
-# This letter pool should reflect the distribution of letters as described in the table below
-# There are only 2 available C letters, so draw_letters cannot ever return more than 2 Cs
-# Since there are 12 Es but only 1 Z, it should be 12 times as likely for the user to draw an E as a Z
-# Invoking this method should not change the pool of letters
-# Imagine that the user returns their hand to the pool before drawing new letters
 
 LETTERS = {
     A: 9,
@@ -55,23 +44,7 @@ def draw_letters()
 end
 
 
-# Wave 2
-# Noor Pseaducode
-# create a method uses_available_letters?(input, letter_in_hand)
-# input = the first parameter, describes some input word, and is a string CHECK IF IT's A STRING
-# letters_in_hand, the second parameter, describes an array of drawn letters in a hand. You can expect this to be an array of ten strings, with each string representing a letter
-# Returns either true or false
-# Returns true if every letter in the input word is available (in the right quantities) in the letters_in_hand
-# Returns false if not; if there is a letter in input that is not present in the letters_in_hand or has too much of compared to the letters_in_hand
-
-# Anna Pseudocode
-# def uses_available_letters?(input, letters_in_hand)
-# set a gate var (valid) to true <-- this means we start off assuming the word is valid
-# separate the input string to an array of strings (each char has a slot in the array)
-# 1) first check that all the letters in the word are in the hand - use set theory (temp - letters_in_hand).size == 0
-# 2) if you pass the first check, for each letter in temp, if the count is > count in letters_in_hand --> valid = false
-# 3) otherwise if you don't pass #1, valid = false
-# return valid (which will be true or false based on the tests above)
+# WAVE 2
 
 def uses_available_letters?(input, letters_in_hand)
 	input = input.chars
@@ -88,4 +61,46 @@ def uses_available_letters?(input, letters_in_hand)
 	end
 end
 
-# wave 3
+# Wave 3
+
+def score_word(word)
+	points = 0
+	# letters_score = {
+	# 		1, ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+	# 		2, ['D', 'G'],
+	# 		3, ['B', 'C', 'M', 'P'],
+	# 		4, ['F', 'H', 'V', 'W', 'Y'],
+	# 		5, ['K'],
+	# 		8, ['J', 'X'],
+	# 		10, ['Q', 'Z']
+	# }
+
+	if word.length > 6
+		points += 8
+	end
+
+	word.each_char do |letter|
+		letter.upcase!
+		if ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].include? (letter)
+			points += 1
+		elsif ['D', 'G'].include? (letter)
+		  points += 2
+		elsif ['B', 'C', 'M', 'P'].include? (letter)
+		  points += 3
+		elsif ['F', 'H', 'V', 'W', 'Y'].include?(letter)
+		  points += 4
+		elsif ['K'].include? (letter)
+		  points += 5
+		elsif ['J', 'X'].include? (letter)
+		  points += 8
+		elsif ['Q', 'Z'].include? (letter)
+		  points += 10
+		end
+	end
+
+	return points
+end
+
+# Wave 4
+# Use score_word function
+# return { word: 'string', score: score_int }
