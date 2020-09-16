00387e2d5d349a0eca153f8d9fe3805fc364f729
Anna Kim <anna.kim93@gmail.com>
2020-09-16 16:13:39 -0400

    fixing weird indentation

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -13,19 +13,19 @@ LETTERS = {
     K: 1,
     L: 4,
     M: 2,
-    N: 6,
-    O: 8,
-	  P: 2,
-	  Q: 1,
-	  R: 6,
-	  S: 4,
-	  T: 6,
-	  U: 4,
-	  V: 2,
-	  W: 2,
-	  X: 1,
-  	Y: 2,
-	  Z: 1
+    N: 6, 
+    O: 8, 
+    P: 2, 
+    Q: 1, 
+    R: 6, 
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
 }
 
 # WAVE 1
@@ -47,58 +47,49 @@ end
 # WAVE 2
 
 def uses_available_letters?(input, letters_in_hand)
-	input = input.chars
+  input = input.chars
 
-	if (input - letters_in_hand).size > 0
-		return false
-	else
-		input.each do |letter|
-			if input.count(letter) > letters_in_hand.count(letter)
-				return false
-			end
-		end
-		return true
-	end
+  if (input - letters_in_hand).size > 0
+    return false
+  else
+    input.each do |letter|
+      if input.count(letter) > letters_in_hand.count(letter)
+        return false
+      end
+    end
+    return true
+  end
 end
 
 # Wave 3
 
 def score_word(word)
-	points = 0
-	# letters_score = {
-	# 		1, ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-	# 		2, ['D', 'G'],
-	# 		3, ['B', 'C', 'M', 'P'],
-	# 		4, ['F', 'H', 'V', 'W', 'Y'],
-	# 		5, ['K'],
-	# 		8, ['J', 'X'],
-	# 		10, ['Q', 'Z']
-	# }
+  points = 0
 
-	if word.length > 6
-		points += 8
-	end
+  if word.length > 6
+    points += 8
+  end
 
-	word.each_char do |letter|
-		letter.upcase!
-		if ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].include? (letter)
-			points += 1
-		elsif ['D', 'G'].include? (letter)
-		  points += 2
-		elsif ['B', 'C', 'M', 'P'].include? (letter)
-		  points += 3
-		elsif ['F', 'H', 'V', 'W', 'Y'].include?(letter)
-		  points += 4
-		elsif ['K'].include? (letter)
-		  points += 5
-		elsif ['J', 'X'].include? (letter)
-		  points += 8
-		elsif ['Q', 'Z'].include? (letter)
-		  points += 10
-		end
-	end
+  word.each_char do |letter|
+    letter.upcase!
+    if ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].include? (letter)
+      points += 1
+    elsif ['D', 'G'].include? (letter)
+      points += 2
+    elsif ['B', 'C', 'M', 'P'].include? (letter)
+      points += 3
+    elsif ['F', 'H', 'V', 'W', 'Y'].include?(letter)
+      points += 4
+    elsif ['K'].include? (letter)
+      points += 5
+    elsif ['J', 'X'].include? (letter)
+      points += 8
+    elsif ['Q', 'Z'].include? (letter)
+      points += 10
+    end
+  end
 
-	return points
+  return points
 end
 
 # Wave 4
