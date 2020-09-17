dbb6163c1dfba2510aeaf0c1c52a84d1be86b54b
Schanen Ryan <schanen.r@gmail.com>
2020-09-16 18:02:07 -0700

    wave 3 added score_word

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -41,3 +41,24 @@ def uses_available_letters?(input, letters_in_hand)
     return true
   end
 end
+
+def score_word(word)
+
+  sum = 0
+  scores_hash = {
+      "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2, "B": 3, "C": 3, "M": 3, "P": 3,
+      "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,"K": 5, "J": 8, "X": 8,"Q": 10, "Z": 10
+  }
+
+  new_arr = word.split("")
+  new_arr.each do |letter|
+    scores_hash.each do |char, score|
+      if letter == char.to_s
+        sum += score
+      end
+    end
+  end
+
+  return sum
+
+end
