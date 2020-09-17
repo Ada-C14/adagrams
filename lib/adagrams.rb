1a75eef48fe3d65cdd95f6938e26b6c92a23466f
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-16 17:21:03 -0700

    define is_in_english_dict? method

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,51 +1,16 @@
-# makes an array of all possible letters
-
+require 'csv'
 
 def draw_letters
-  # p letter_pool
-  letter_pool = []
-  letter_pool_hash = {
-      A: 9,
-      B: 2,
-      C: 2,
-      D: 4,
-      E: 12,
-      F: 2,
-      G: 3,
-      H: 2,
-      I: 9,
-      J: 1,
-      K: 1,
-      L: 4,
-      M: 2,
-      N: 6,
-      O: 8,
-      P: 2,
-      Q: 1,
-      R: 6,
-      S: 4,
-      T: 6,
-      U: 4,
-      V: 2,
-      W: 2,
-      X: 1,
-      Y: 2,
-      Z: 1
-  }
-
-  letter_pool_hash.each do |letter, frequency|
-    frequency.times { letter_pool << letter.to_s }
-  end
-
+  letter_pool = %W( #{ "A" * 9 + "B" * 2 + "C" * 2 + "D" * 4 + "E" * 12 + "F" * 2 + "G" * 3 + "H" * 2 + "I" * 9 + "J" * 1 + "K" * 1 + "L" * 4 + "M" * 2 + "N" * 6 + "O" * 8 + "P" * 2 + "Q" * 1 + "R" * 6 + "S" * 4 + "T" * 6 + "U" * 4 + "V" * 2 + "W" * 2 + "X" * 1 + "Y" * 2 + "Z" * 1} ).join.split("")
   random_letter = letter_pool.shuffle.take(10)
   return random_letter
 end
 
 def uses_available_letters?(input, letters_in_hand)
   letters_in_hand_copy = letters_in_hand.clone
-  input.each_char do |letter|
+  input.upcase.each_char do |letter|
     if letters_in_hand_copy.include?(letter)
-      letters_in_hand_copy.delete(letter)
+      letters_in_hand_copy.delete_at(letters_in_hand_copy.index(letter))
     else
       return false
     end
@@ -89,7 +54,6 @@ def highest_score_from(words)
 
   high_score = words.max_by { |word| score_word(word) }
   high_words = words.select { |word| score_word(word) == score_word(high_score) }
-  # high_words.sort_by! { |word| word.length }
 
   if high_words.length == 1
     winner[:word] = high_words[0]
@@ -106,4 +70,9 @@ def highest_score_from(words)
   end
 
   return winner
+end
+
+def is_in_english_dict?(input)
+  dictionary = CSV.read('../adagrams/assets/dictionary-english.csv').flatten
+  return dictionary.include?(input.downcase)
 end
