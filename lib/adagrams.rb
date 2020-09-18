048d6c28b2d33f45cf87f66a25e130072896de49
Schanen Ryan <schanen.r@gmail.com>
2020-09-18 12:15:06 -0700

    added waves 4 and optional wave 5

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,4 +1,4 @@
-
+require 'csv'
 
 def draw_letters
   letters_count = {"A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1}
@@ -15,7 +15,6 @@ def draw_letters
 
 end
 
-
 def uses_available_letters?(input, letters_in_hand)
   counter = 0
   counts = Hash.new(0)
@@ -24,7 +23,7 @@ def uses_available_letters?(input, letters_in_hand)
   end
 
   input.split("").each do |letter|
-    if letters_in_hand.include?(letter)
+    if letters_in_hand.include?(letter.upcase)
       if counts[letter] > 0
         counts[letter] -= 1
       elsif counts[letter] == 0
@@ -53,12 +52,77 @@ def score_word(word)
   new_arr = word.split("")
   new_arr.each do |letter|
     scores_hash.each do |char, score|
-      if letter == char.to_s
+      if letter.upcase == char.to_s
         sum += score
       end
     end
   end
-
+  if new_arr.length > 6
+    sum += 8
+  end
   return sum
 
 end
+
+def highest_score_from(words)
+  word_scores = Array.new
+  words.each do |word|
+      word_scores << {:word => word, :score => score_word(word)}
+  end
+
+  score_arr = []
+  max_score = 0
+  word_scores.each_with_index do |pair, index|
+    if word_scores[index][:score] > max_score
+      max_score = word_scores[index][:score]
+    end
+  end
+  word_scores.each_with_index do |pair, index|
+    if word_scores[index][:score] == max_score
+        score_arr << word_scores[index]
+    end
+  end
+
+  best_word = Hash.new
+  min_length = 10
+
+  if score_arr.length > 1
+    score_arr.each do |score|
+      if score[:word].length == 10
+        best_word = score
+        return best_word
+      end
+    end
+
+    score_arr.each do |score|
+      if score[:word].length < min_length
+        min_length = score[:word].length
+      end
+    end
+
+    score_arr.each do |score|
+      if score[:word].length == min_length
+        best_word = score
+        return best_word
+      end
+    end
+  else
+    best_word = score_arr[0]
+    return best_word
+  end
+  return best_word
+end
+
+input = "a"
+
+def is_in_english_dict?(input)
+  dictionary = CSV.read('../assets/dictionary-english.csv', headers: true).map do |word|
+   word.to_s.chomp
+  end
+  return dictionary.include?(input.downcase)
+end
+
+
+
+
+
