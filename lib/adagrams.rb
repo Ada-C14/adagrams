3b337215ed96d7f2f20fef663b24643e5838c549
beauttie <bak02013@pomona.edu>
2020-09-17 20:16:22 -0700

    Added draw_letters method

---
+++ lib/adagrams.rb
@@ -0,0 +1,37 @@
+# Wave 1
+LETTER_DISTRIB = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
+}
+
+def draw_letters
+  letter_pool = LETTER_DISTRIB.flat_map { |letter_symbol, freq| [letter_symbol.to_s] * freq }
+
+  user_hand = letter_pool.sample(10)
+
+  return user_hand
+end
