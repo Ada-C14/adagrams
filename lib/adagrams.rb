d3489c2146e2894970a7c588bade30e35f3fd082
Sophie Messing <sophie.e.messing@gmail.com>
2020-09-18 09:58:18 -0700

    cleaned up line spacing

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -27,6 +27,7 @@ def draw_letters
       Y: 2,
       Z: 1,
   }
+
   distribution_array = []
   distribution.each do |letter, number|
     number.times do
@@ -50,19 +51,20 @@ def uses_available_letters?(input, letters_in_hand)
   input.each do |letter|
     input_hash[letter] += 1
   end
-
   input = input_hash
+
   letter_hash = Hash.new(0)
   letters_in_hand.each do |letter|
     letter_hash[letter] += 1
-
   end
   letters_in_hand = letter_hash
+
   input.each do |letter, count|
     if letters_in_hand[letter] < count
       return false
     end
   end
+
   return true
 end
 
@@ -79,8 +81,8 @@ def score_word(word)
   word.each do |letter|
     hash[letter] += 1
   end
-
   word = hash
+
   values = {
       "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1,
       "D" => 2, "G" => 2,
@@ -89,14 +91,14 @@ def score_word(word)
       "K" => 5,
       "J" => 8, "X" => 8,
       "Q" => 10, "Z" => 10
-  }
+      }
 
   values.each do |letter, value|
     score += (word[letter] * values[letter])
   end
+
   return score
 end
-
 
 def highest_score_from(words)
 
@@ -108,8 +110,8 @@ def highest_score_from(words)
   words.each do |word|
     score = score_word(word)
     if score > best_score
-      best_score = score
       winning_word = word
+      best_score = score
     elsif word.length == 10 && score == best_score && winning_word.length < 10
       winning_word = word
       best_score = score
