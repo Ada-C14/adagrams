d324cc7c3c7a143a1f7e9c34bff125bd71f5bcd3
codeandcodemore <alicedilanchian@gmail.com>
2020-09-18 13:45:59 -0500

    Wave 1 complete

---
+++ lib/adagrams.rb
@@ -0,0 +1,33 @@
+# WAVE 1
+def draw_letters
+  letter_pool = %w(
+  A A A A A A A A A
+  B B
+  C C
+  D D D D
+  E E E E E E E E E E E E
+  F F
+  G G G
+  H H
+  I I I I I I I I I
+  J
+  K
+  L L L L
+  M M
+  N N N N N N
+  O O O O O O O O
+  P P
+  Q
+  R R R R R R
+  S S S S
+  T T T T T T
+  U U U U
+  V V
+  W W
+  X
+  Y
+  Z
+)
+  return letter_pool.sample(10)
+
+end
