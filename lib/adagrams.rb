a3472470fae9c29e9551eddd5f07662c50a7b49e
Iris Lux <iris.lux0@gmail.com>
2020-09-17 22:50:01 -0700

    Fixed bug in uses_available_letters?

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,16 +1,5 @@
 require 'csv'
-#require 'awesome_print'
 
-# - No parameters
-# - Returns an array of ten strings
-# - Each string should contain exactly one letter
-# - These represent the hand of letters that the player has drawn
-# - The letters should be randomly drawn from a pool of letters
-# - This letter pool should reflect the distribution of letters as described in the table below
-# - There are only 2 available `C` letters, so `draw_letters` cannot ever return more than 2 `C`s
-# - Since there are 12 `E`s but only 1 `Z`, it should be 12 times as likely for the user to draw an `E` as a `Z`
-# - Invoking this method should **not** change the pool of letters
-# - Imagine that the user returns their hand to the pool before drawing new letters
 
 def draw_letters
   hand = Array.new(10)
@@ -58,52 +47,56 @@ def draw_letters
   return hand
 end
 
-# To do so, add a method called `uses_available_letters?` in `adagrams.rb`. This method should have the following properties:
-#
-#- Has two parameters:
-#   - `input`, the first parameter, describes some input word, and is a string
-# - `letters_in_hand`, the second parameter, describes an array of drawn letters in a hand. You can expect this to be an array of ten strings, with each string representing a letter
-# - Returns either `true` or `false`
-# - Returns `true` if every letter in the `input` word is available (in the right quantities) in the `letters_in_hand`
-# - Returns `false` if not; if there is a letter in `input` that is not present in the `letters_in_hand` or has too much of compared to the `letters_in_hand`
-#
-
 def uses_available_letters?(input, letters_in_hand)
 
-  # raise ArgumentError.new("input is nil") if input.nil?
-  #raise ArgumentError.new("input is nil") if letters_in_hand.nil?
+  # create a hash where each letter is the key and the number of letters is the value.
+  # take the intersection_with dupes, turn into a hash of letters and counts
+  # Take input_array, turn into hash of letters and counts
+  # if the length two hashes is the same ()all letters match) compare the letter count between letters
+  # if INput_hash letter count is more, return false
 
+  raise ArgumentError.new("input is nil") if input.nil?
+  raise ArgumentError.new("input is nil") if letters_in_hand.nil?
+  result = ''
+  # input.length.times do |index|
+  #   if letters_in_hand.include?input[index]
+  #     if letters_in_hand.select { |letter| letter == input[index] }.count == input[index].count
+  #       result = true
+  #     else
+  #      result = false
+  #   end
+  # end
+
+  #return result
 
   input_array = input.upcase.split("").sort
-  #sorted_hand = letters_in_hand.sort[0...input_array.length]
-  intersection = input_array & letters_in_hand
-  subtracted = letters_in_hand - intersection
-  subsubtracted = letters_in_hand - subtracted
+  intersection_with_dupes = (letters_in_hand - (letters_in_hand - (input_array & letters_in_hand)))
+  input_letter_count = {}
+  inter_letter_count = {}
+  input_array.each do |letter|
+    input_letter_count[letter.to_sym] = input_array.count(letter)
+  end
 
-  #remove all the letters that aren't in input
-  return input_array.sort == subsubtracted.sort
+  intersection_with_dupes.each do |letter|
+    inter_letter_count[letter.to_sym] = intersection_with_dupes.count(letter)
+  end
 
+  uses_available = true
+  if(input_letter_count.length == inter_letter_count.length)
+    input_letter_count.each do |letter, count|
+      uses_available = uses_available && count <= inter_letter_count[letter]
+    end
+  else
+    uses_available = false
+  end
+
+  # #variable contains intersection, but retains duplicate letters
+
+  #
+  # #remove all the letters that aren't in input
+  # return input_array == intersection_with_dupes
+  return uses_available
 end
-
-
-# Name this method `score_word` in `adagrams.rb`. This method should have the following properties:
-# - Has one parameter: `word`, which is a string of characters
-# - Returns an integer representing the number of points
-# - Each letter within `word` has a point value. The number of points of each letter is summed up to represent the total score of `word`
-# - Each letter's point value is described in the table below
-# - If the length of the word is 7, 8, 9, or 10, then the word gets an additional 8 points
-
-#### Score chart
-
-# |Letter                        | Value|
-# |:----------------------------:|:----:|
-# |A, E, I, O, U, L, N, R, S, T  |   1  |
-# |D, G                          |   2  |
-# |B, C, M, P                    |   3  |
-# |F, H, V, W, Y                 |   4  |
-# |K                             |   5  |
-# |J, X                          |   8  |
-# |Q, Z                          |   10 |
 
 def score_word(word)
 
@@ -134,18 +127,6 @@ def score_word(word)
 
   return total_score
 end
-
-# Add a method called `highest_score_from` in `adagrams.rb`. This method should have the following properties:
-#
-# - Has one parameter: `words`, which is an array of strings
-# - Returns a single hash that represents the data of a winning word and its score. The hash should have the following keys:
-# - `:word`, whose value is a string of a word
-# - `:score`, whose value is the score of that word
-# - In the case of tie in scores, use these tie-breaking rules:
-#     - prefer the word with the fewest letters...
-#     - ...unless one word has 10 letters. If the top score is tied between multiple words and one is 10 letters long, choose the one with 10 letters over the one with fewer tiles
-#     - If the there are multiple words that are the same score and the same length, pick the first one in the supplied list
-
 
 def highest_score_from(words)
 
@@ -179,3 +160,5 @@ def is_in_english_dict?(input)
 
 end
 
+
+p uses_available_letters?('HEN', %w(H N N N L E H K Y U))
